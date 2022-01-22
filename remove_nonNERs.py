861b663996b53037db0bf9c704e079c69aac0f2d
chen-bryan <ubuntu@ip-172-31-10-165.us-west-2.compute.internal>
2022-01-22 00:28:10 +0000

    Fixed and reduced threshold to 15

--- remove_nonNERs.py
+++ remove_nonNERs.py
@@ -20,8 +20,9 @@ with open('/home/ubuntu/relationship_visualizer/NERs.txt', 'r') as f:
     for line in f:
         NERs.append(line.replace('\n', ''))
 
-NERs = {k: v for k, v in dict(Counter(NERs)).items() if v >= 50}
+NERs = {k: v for k, v in dict(Counter(NERs)).items() if v >= 15}
 NERs = list(NERs.keys())
+print(len(NERs))
 
 keys = set(embeddings_dict.keys())
 failed = []
@@ -30,16 +31,17 @@ def get_vector_average(word):
     try:
         vector = embeddings_dict[split_word[0]]
     except:
-        print(split_word[0])
+        print("fail1" + split_word[0])
         failed.append(word)
         return None
     for indiv in split_word[1:]:
         try:
             vector = vector + embeddings_dict[indiv] 
         except:
-            print(word)
+            # print(word)
             failed.append(indiv)
             return None
+    print( word + " " + np.array2string(vector))
     return vector / len(split_word)
 
 present = {}
@@ -47,8 +49,6 @@ present = {}
 counter = 0
 for x in NERs:
     counter += 1
-    if counter % 100 == 0:
-        print(counter)
 
     avg_vector = get_vector_average(x)
     for ele in keys:
@@ -56,6 +56,7 @@ for x in NERs:
             present.update({x: avg_vector})
 
 print('4')
+
 with open('/home/ubuntu/relationship_visualizer/GloVe/failed.txt', 'w') as w:
     w.write(' '.join(failed))
 with open('/home/ubuntu/relationship_visualizer/GloVe/NER_vectors.txt', 'w') as f:
